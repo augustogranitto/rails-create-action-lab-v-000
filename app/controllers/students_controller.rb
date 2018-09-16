6becb4d517c6f1833274efd6fc65c90ea9b841fa
Augusto Granitto <augustogranitto@gmail.com>
2018-09-16 08:08:33 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,6 @@ class StudentsController < ApplicationController
     @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
     @student.save
     redirect_to student_path(@student)
-
   end
 
 end
